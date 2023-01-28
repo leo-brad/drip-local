a7637d73636d55ce4829d7580adc01546fe70d3a
Eric Hart <>
2023-01-28 21:53:54 +0800

    [Add]

--- src/render/script/component/Content/index.js
+++ src/render/script/component/Content/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import style from './index.module.css';
 import OfflineComponent from '~/render/script/component/OfflineComponent';
+import check from '~/render/script/lib/check';
 import global from '~/render/script/obj/global';
 
 const {
@@ -18,13 +20,15 @@ class Content extends OfflineComponent {
     this.state = {
       instance,
     };
+    this.id = new Date().getTime().toString();
     this.instance = instance;
     this.updateView = this.updateView.bind(this);
     this.dealEvent = this.dealEvent.bind(this);
+    this.checkContent = this.checkContent.bind(this);
     this.syncInstance = this.syncInstance.bind(this);
   }
 
-  updateView() {
+  async updateView() {
     const {
       share: {
         focus,
@@ -35,8 +39,29 @@ class Content extends OfflineComponent {
       this.setState({
         instance,
       });
+      await check(this.checkContent);
       this.sendMountAndUnmount();
     }
+  }
+
+  getDom() {
+    const { dom, } = this;
+    if (dom === undefined) {
+      const { id, } = this;
+      this.dom = document.getElementById(id);
+    }
+    return this.dom;
+  }
+
+  checkContent() {
+    const dom = this.getDom();
+    let ans = false;
+    if (dom) {
+      if (dom.children.length !== 0) {
+        ans = true;
+      }
+    }
+    return ans;
   }
 
   dealEvent(data) {
@@ -83,6 +108,7 @@ class Content extends OfflineComponent {
   }
 
   render() {
+    const { id, } = this;
     const {
       instance,
     } = this.state;
@@ -103,7 +129,9 @@ class Content extends OfflineComponent {
     } else {
       component[instance] = null;
     }
-    return component[instance];
+    return(
+      <div id={id} className={style.content}>{component[instance]}</div>
+    );
   }
 }
 
