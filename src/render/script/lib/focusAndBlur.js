c32f41dfae067e309ba5e61476ca3d14765bd1f6
Eric Hart <>
2023-01-31 15:18:46 +0800

    [Add]

--- src/render/script/lib/focusAndBlur.js
+++ src/render/script/lib/focusAndBlur.js
@@ -1,5 +1,16 @@
 import global from '~/render/script/obj/global';
 
+async function block() {
+  await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 750);
+  });
+  if (!global.share.focus) {
+    block();
+  }
+}
+
 export default function onlineAndOffline() {
   const {
     share: {
@@ -23,5 +34,6 @@ export default function onlineAndOffline() {
     } else {
       blurTimes += 1;
     }
+    block();
   });
 }
